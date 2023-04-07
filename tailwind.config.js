f1dbbef3d8bc96cc19d15aa325ef7eadba98c02d
mattrhummel <matt@matthummel.com>
2023-04-07 09:59:23 -0400

    fix landmarks

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/*.{html,js}",
-  "/node_modules/flowbite/**/*.js"
+    './src/**/*.{html,js}',
+    'index.html',
+    "/node_modules/flowbite/**/*.js"
 ],
   
   theme: {
